6bb66b91b3793dddb693e44f49d4c8436b516d48
mxou <maximilien.dlrm@gmail.com>
2024-08-02 12:57:25 +0200

    ajout manifests/service worker pour PWA + ajustements css

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,4 +96,48 @@ document.addEventListener('DOMContentLoaded', () => {
     
     // Ajoute un gestionnaire de clic pour le bouton de réinitialisation
     resetButton.addEventListener('click', resetGame);
+
+if ('serviceWorker' in navigator) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register('sw.js')
+                    .then(registration => {
+                        console.log('Service Worker registered with scope:', registration.scope);
+                    })
+                    .catch(err => {
+                        console.log('Service Worker registration failed:', err);
+                    });
+            });
+        }
+
+        let deferredPrompt;
+        const installButton = document.getElementById('install-button');
+
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Empêcher le navigateur de montrer le prompt par défaut
+            e.preventDefault();
+            // Stocker l'événement pour le déclencher plus tard
+            deferredPrompt = e;
+            // Afficher le bouton d'installation
+            installButton.style.display = 'block';
+
+            installButton.addEventListener('click', () => {
+                // Masquer le bouton d'installation
+                installButton.style.display = 'none';
+                // Afficher le prompt d'installation
+                deferredPrompt.prompt();
+                // Attendre que l'utilisateur réponde au prompt
+                deferredPrompt.userChoice.then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('User accepted the A2HS prompt');
+                    } else {
+                        console.log('User dismissed the A2HS prompt');
+                    }
+                    deferredPrompt = null;
+                });
+            });
+        });
+
+        window.addEventListener('appinstalled', (evt) => {
+            console.log('PWA was installed');
+        });
 });
